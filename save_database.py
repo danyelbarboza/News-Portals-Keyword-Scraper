588176a5f5cec4efa3e3c7177e323ffa780d1e17
Danyel Barboza <dhgbarboza@gmail.com>
2025-05-23 20:59:53 -0300

    Insere no banco de dados apenas noticias com links unicos

--- save_database.py
+++ save_database.py
@@ -17,12 +17,19 @@ class Database():
         )
     
     def insert_news(self, news_list, portal_name):
+        inserted_count = 0
         try:
             with self.connection.cursor() as cursor:
                 sql = f"INSERT INTO {portal_name} (title, link, scraping_date, news_date, article) VALUES (%s, %s, %s, %s, %s)"
                 for news in news_list:
-                    cursor.execute(sql, (news['title'], news['link'], news['scraping_date'], news['news_date'], news['article']))
-            self.connection.commit()
-            print(f"\nColeta finalizada ({len(news_list)} notícias salvas no banco de dados)\n")
+                    try:
+                        cursor.execute(sql, (news['title'], news['link'], news['scraping_date'], news['news_date'], news['article']))
+                        inserted_count += 1
+                    except psycopg2.errors.UniqueViolation:
+                        print(f"Notícia duplicada: {news['title']}")
+                        self.connection.rollback() # Desfaz a operação
+                    else:
+                        self.connection.commit()
+            print(f"\nColeta finalizada: ({len(news_list)} notícias coletadas e {inserted_count} inseridas com sucesso.)\n")
         except Exception as e:
             print(f"Erro ao inserir notícia: {e}")
